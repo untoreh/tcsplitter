fc65f71052db35b7bc57aade43d5a74f66c0b089
untoreh <bassobassista@gmail.com>
2018-01-20 07:28:39 +0100

    updated scripts

--- utils/tcs-client.sh
+++ utils/tcs-client.sh
@@ -1,26 +1,37 @@
 #!/bin/sh
 
-#remoteip=148.251.3.246
-remoteip=127.0.0.1
-pl=10000
+remoteip=88.198.69.215
+pl=1292
 mtu=1308
-#pl=8000
-#pl=500
-#conns=100
-conns=4
-#addr=192.168.42.129:6000
-addr=127.0.0.1:6000
-proto=udp
-proto=tcp
+# pl=8000
+# pl=500
+conns=1
+# conns=4
+# skip=$((conns*4))
+bt=$((1000/(conns)))
+to=$bt
+ti=$((bt/2))
+lassoes=0
+# addr=192.168.42.129:6000
+addr=localhost
+# proto=udp
 
 ## base
 listen_port=6000
-rfling_port=20900
-sync_port=20902
+rfling_port=10900
+# rfling_port=80
+sync_port=10902
+
+## extra
+# fling_port=20900
 
 exec ./tcsplitter \
-        --listen $addr \
-        --conns $conns \
-        --frags $pl:$mtu \
-        --rFling $remoteip:$rfling_port \
-        --rSync $remoteip:$sync_port
+     --frags $pl:$mtu \
+     --conns $conns \
+     --buffer $bt:0 \
+     --tick $ti \
+     --tock $to \
+     --listen $addr \
+     --rFling $remoteip:$rfling_port \
+     --rSync $remoteip:$sync_port
+
--- utils/tcs-server.sh
+++ utils/tcs-server.sh
@@ -1,31 +1,35 @@
 #!/bin/sh
 
-listenip=127.0.0.1
-pl=10000
+# listenip=10.10.12.209
+listenip=localhost
+pl=1292
 mtu=1308
-#pl=11756 # (+16)
-#pl=8000
-#pl=500
-#conns=100
-conns=4
-proto=udp
-proto=tcp
+# pl=11756 # (+16)
+# pl=8000
+# pl=500
+conns=12
+bt=$((1000/conns))
+to=$bt
+ti=$((bt/2))
+# proto=udp
 
-#fling_port=8080
-fling_port=20900
-lasso_port=80
-rfling_port=20901
-sync_port=20902
+# fling_port=80
+fling_port=10900
+# lasso_port=80
+# rfling_port=20901
+sync_port=30902
 listen_port=6001
 
-#forward="live-mil.twitch.tv:1935"
-forward=127.0.0.1:6003
+# forward="live-mil.twitch.tv:1935"
+forward=127.0.0.1:22
 
 exec ./tcsplitter \
---conns $conns \
---frags $pl:$mtu \
---listen $listenip:$listen_port \
---forward $forward \
---lFling $listenip:$fling_port \
---rFling 0 \
---lSync $listenip:$sync_port
+     --frags $pl:$mtu \
+     --conns $conns \
+     --buffer $bt:0 \
+     --tick $ti \
+     --tock $to \
+     --listen $listenip:$listen_port \
+     --forward $forward \
+     --lFling $listenip:$fling_port \
+     --lSync $listenip:$sync_port
